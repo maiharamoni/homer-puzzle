88c02eab9b55751bc37228a9aa0801808ca0886e
Mai Haramoni <maiharamoni@gmail.com>
2023-03-12 17:43:59 +0000

    "change the image to upload on the game"

---
+++ assets/js/puzzle.js
@@ -0,0 +1,24 @@
+
+var rows = 3;
+var columns = 3;
+
+var currTile;
+var otherTile; //blank tile
+
+var turns = 0;
+
+var imgOrder = ["1", "2", "3", "4", "5", "6", "7", "8", "9"];
+
+window.onload = function() { 
+    for (let r=0; r < rows; r++) {
+        for (let c=0; c < columns; c++){
+
+            // <img id="0-0" src="1.jpg">
+            let tile = document.createElement("img");
+            tile.id = r.toString() + "-" + c.toString();
+            tile.src = imgOrder.shift() + ".jpg";
+
+            document.getElementById("board").append(tile);
+        }
+    }
+}
